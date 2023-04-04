06c7405611c7ab1afbf4ec3705b6975f1156a15a
korneev.dev <korneev.dev@gmail.com>
2023-04-05 00:04:24 +0300

    Refactor: delete default layout

---
+++ app/src/main/java/github/mik0war/hinote/presentation/NotesActivity.kt
@@ -0,0 +1,12 @@
+package github.mik0war.hinote.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import github.mik0war.hinote.R
+
+class NotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

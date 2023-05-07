8dd8d2d7c5cb8e96933c452a0a49ac1ade8bd4c3
korneev.dev <korneev.dev@gmail.com>
2023-05-07 23:39:55 +0300

    Add markup to create note layout

--- app/src/main/java/github/mik0war/hinote/presentation/NoteCreateFragment.kt
+++ app/src/main/java/github/mik0war/hinote/presentation/NoteCreateFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import github.mik0war.hinote.R
 
 class NoteCreateFragment : Fragment() {
@@ -18,7 +18,7 @@ class NoteCreateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.toListButton)
+        val button = view.findViewById<FloatingActionButton>(R.id.createButton)
 
         button.setOnClickListener{
             findNavController().navigate(R.id.action_CreateNoteFragment_to_NotesListFragment)

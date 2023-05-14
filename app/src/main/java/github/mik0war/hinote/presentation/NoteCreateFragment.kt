50574a54b71c9de92e829958f00a7f732c9bb735
korneev.dev <korneev.dev@gmail.com>
2023-05-15 01:36:08 +0300

    Note creating business logic

--- app/src/main/java/github/mik0war/hinote/presentation/NoteCreateFragment.kt
+++ app/src/main/java/github/mik0war/hinote/presentation/NoteCreateFragment.kt
@@ -4,23 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import github.mik0war.hinote.NotesApp
 import github.mik0war.hinote.R
 
 class NoteCreateFragment : Fragment() {
+    private lateinit var viewModel: NoteViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_create_note, container, false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = (requireActivity().application as NotesApp).viewModel
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val button = view.findViewById<FloatingActionButton>(R.id.createButton)
 
+        val headerEditText = view.findViewById<EditText>(R.id.noteHeader)
+        val bodyEditText = view.findViewById<EditText>(R.id.noteBody)
+        var i = 0
+
         button.setOnClickListener{
+            viewModel.createNote(i++, headerEditText.text.toString(), bodyEditText.text.toString(), "$i$i:$i$i:$i$i")
             findNavController().navigate(R.id.action_CreateNoteFragment_to_NotesListFragment)
         }
     }
